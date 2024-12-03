a0d15f9c234fc321944ca8563d5e8d0cabc97ffa
Nicolas020807 <nicolaslegal720@gmail.com>
2024-12-03 13:47:02 -0300

    Add files via upload

---
+++ stories.js
@@ -0,0 +1,59 @@
+var crl1 = document.getElementById('myCrl1').getContext('2d');
+
+crl1.beginPath()
+crl1.arc(60,100,40,0,2 *Math.PI); //criar circulo
+crl1.fillStyle = "#D20103";   // cor do circulo 
+crl1.fill();
+crl1.linewidth = 5;//expressura da linha
+crl1.strokeStyle = "#dbdbdb";
+crl1.stroke();
+
+var crl2 = document.getElementById('myCrl2').getContext('2d');
+
+crl2.beginPath()
+crl2.arc(60,100,40,0,2 *Math.PI); //criar circulo
+crl2.fillStyle = "#FFF2F2";   // cor do circulo 
+crl2.fill();
+crl2.linewidth = 5; //expressura da linha
+crl2.strokeStyle = "#dbdbdb";
+crl2.stroke();
+
+var crl3 = document.getElementById('myCrl3').getContext('2d');
+
+crl3.beginPath()
+crl3.arc(60,100,40,0,2 *Math.PI); //criar circulo
+crl3.fillStyle = "#010000";   // cor do circulo 
+crl3.fill();
+crl3.linewidth = 5; //expressura da linha
+crl3.strokeStyle = "#dbdbdb";
+crl3.stroke();
+
+var crl4 = document.getElementById('myCrl4').getContext('2d');
+
+crl4.beginPath()
+crl4.arc(60,100,40,0,2 *Math.PI); //criar circulo
+crl4.fillStyle = "#FDF103";   // cor do circulo 
+crl4.fill();
+crl4.linewidth = 5; //expressura da linha
+crl4.strokeStyle = "#dbdbdb";
+crl4.stroke();
+
+var crl5 = document.getElementById('myCrl5').getContext('2d');
+
+crl5.beginPath()
+crl5.arc(60,100,40,0,2 *Math.PI); //criar circulo
+crl5.fillStyle = "#737371";   // cor do circulo 
+crl5.fill();
+crl5.linewidth = 5 //expressura da linha
+crl5.strokeStyle = "#dbdbdb";
+crl5.stroke();
+
+var crl6 = document.getElementById('myCrl6').getContext('2d');
+
+crl6.beginPath()
+crl6.arc(60,100,40,0,2 *Math.PI); //criar circulo
+crl6.fillStyle = "#BDA703";   // cor do circulo 
+crl6.fill();
+crl6.linewidth = 5; //expressura da linha
+crl6.strokeStyle = "#dbdbdb";
+crl6.stroke();
